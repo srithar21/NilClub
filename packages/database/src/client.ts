32e4b7eed201027726756c17560c7e350a40f5fa
Srithar Rajendran <srithar21@gmail.com>
2026-06-02 13:32:00 -0600

    Added the packages/database connection and libraries

---
+++ packages/database/src/client.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+
+config({ path: '../../.env' });
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle(sql);
